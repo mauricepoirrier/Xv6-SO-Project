aba8f7b5244b99ad1739df099b03d8963859da46
mauricepoirrier <mauricepoirrier@gmail.com>
2018-06-25 22:17:01 -0400

    Tarea 3 real done

--- sysproc.c
+++ sysproc.c
@@ -89,3 +89,9 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+int
+sys_getPhysicalAdress(void)
+{
+  return physicalAdressProcessing();
+}
--- user.h
+++ user.h
@@ -23,7 +23,7 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
-
+int getPhysicalAdress(void);
 // ulib.c
 int stat(char*, struct stat*);
 char* strcpy(char*, char*);
